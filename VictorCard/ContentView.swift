41c26152544ef82d4b06b8df073161184d95e367
Estudiante 1 <45402024+victortejeda@users.noreply.github.com>
2022-09-05 02:19:30 -0400

    Initial Commit

---
+++ VictorCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VictorCard
+//
+//  Created by Victor Tejeda on 5/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VictorCard/VictorCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VictorCardApp.swift
+//  VictorCard
+//
+//  Created by Victor Tejeda on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct VictorCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
